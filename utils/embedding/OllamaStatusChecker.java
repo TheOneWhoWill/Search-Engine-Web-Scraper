13664eefee1b4b113f64181fb0bbced3e7ce91bd
TheOneWhoWil <norikrafikov3456@gmail.com>
2025-02-19 02:30:46 -0500

    Fixing vs code visual glitch

--- utils/embedding/OllamaStatusChecker.java
+++ utils/embedding/OllamaStatusChecker.java
@@ -6,6 +6,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
