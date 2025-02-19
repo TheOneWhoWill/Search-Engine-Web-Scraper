432b8dc17307f2dfd8bc8d2c5a45c5c3f609f23b
TheOneWhoWil <norikrafikov3456@gmail.com>
2025-02-19 02:13:32 -0500

    Improvements to the compiler

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM Create the build directory if it doesn't exist
-if not exist build mkdir build
-javac -d build Main.java
+rmdir /s /q build
+mkdir build
 REM Compile all.java files in the utils directory
-javac -d build -cp "modules/*" utils\*.java
+javac -d build -cp "modules/*" utils\*.java utils\crawler\*.java utils\embedding\*.java Main.java
 REM Run the Main class.  Adjust the classpath and Main class name if necessary.
 java -cp "build;modules/*" Main
